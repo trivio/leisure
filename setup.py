9fc4f15a491c3108fa1d6820943a5fc39cc155b6
Scott Robertson <srobertson@codeit.com>
2013-04-10 08:21:35 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os.path
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README_PATH = os.path.join(HERE, 'README.md')
+try:
+    README = open(README_PATH).read()
+except IOError:
+    README = ''
+
+
+setup(
+  name='leisure',
+  py_modules = ['leisure'],
+  version='0.0.1',
+  description='local job runner for disco',
+  long_description=README,
+  author='Scott Robertson',
+  author_email='scott@triv.io',
+  url='http://github.com/trivio/leisure',
+  classifiers=[
+      "Programming Language :: Python",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development",
+  ],
+  entry_points={
+    'console_scripts': [
+      'leisure = leisure:main'
+    ]
+  }
+)
