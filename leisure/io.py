27aaec3d095f0200cf4a05508216f087a672720e
Scott Robertson <srobertson@codeit.com>
2013-04-22 15:06:20 -0700

    Added a little color to log messages

--- leisure/io.py
+++ leisure/io.py
@@ -1,14 +1,23 @@
 import sys
+from colorama import init, Fore, Back
+init()
 
-def puts(msg, write=sys.stdout.write):
+def puts(msg, write=sys.stdout.write, fore=None, back=None):
   """Displays a string for the user"""
+  if fore:
+    fore = getattr(Fore, fore.upper())
+  else:
+    fore = ''
+
+
   lines = msg.splitlines()
-  write("----> " + lines.pop(0) + '\n')
+  write("----> " + fore + lines.pop(0) + '\n')
 
   for line in lines:
     write("     ")
     write(line)
     write('\n')
+  write(Fore.RESET)
 
 def indent(msg, write=sys.stdout.write):
   for line in msg.splitlines():
